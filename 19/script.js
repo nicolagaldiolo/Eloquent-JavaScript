1b57d29f3e65ad6fc7b9e39af115ce414e16c8fc
Nicola Galdiolo <galdiolo.nicola@gmail.com>
2017-09-18 07:47:06 +0200

    paint program progress

--- 19/script.js
+++ 19/script.js
@@ -18,7 +18,7 @@ function elt(name, attributes) {
 var controls = Object.create(null);
 
 function createPaint(parent) {
-  var canvas = elt("canvas", {width: 500, height: 300});
+  var canvas = elt("canvas", {width: 800, height: 500});
   var cx = canvas.getContext("2d");
   var toolbar = elt("div", {class: "toolbar"});
   //console.log(controls);
@@ -34,7 +34,12 @@ var tools = Object.create(null);
 controls.tool = function(cx) {
   var select = elt("select");
   for (var name in tools){
-    select.appendChild(elt("option", null, name));
+    if(name == "Flood fill"){
+      select.appendChild(elt("option", {selected:""}, name));
+    }else{
+      select.appendChild(elt("option", null, name));
+    }
+    
   }
 
   cx.canvas.addEventListener("mousedown", function(event) {
@@ -244,6 +249,7 @@ tools.Rectangle = function(event, cx) {
 
 function colorAt(cx, x, y){
   var data = cx.getImageData(x, y, 1, 1).data;
+  //console.log(data);
   var color = "rgb(" + data[0] + "," + data[1] + "," + data[2] + ")";
   return rgb2hex(color);
 }
@@ -275,3 +281,109 @@ tools["Pick color"] = function(event, cx) {
   colorPicker.value = color;
 
 };
+
+function coloraVicino(f){
+  f()
+  console.log("coloro il vicino");
+}
+
+function controllaStessoColore(pos1, pos2, data){
+  // funzione che controlla 2 pixel vicini e mi torna se sono uguali
+  var sameColor = true;
+  var offset1 = (pos1.x + pos1.y * data.width) * 4;
+  var offset2 = (pos2.x + pos2.y * data.width) * 4;
+  
+  for(var i = 0; i < 4; i++){
+    if(data.data[offset1 + i] !== data.data[offset2 + i]){
+      sameColor = false;
+      break;
+    }
+  }
+  return sameColor;
+}
+
+
+tools["Flood fill"] = function(event, cx) {
+  var startPos = relativePos(event, cx.canvas);
+  
+  var dataImage = cx.getImageData(0,0,cx.canvas.width, cx.canvas.height);
+  
+  var alreadyFilled = new Array(dataImage.width * dataImage.height);
+  
+  var workList = [startPos];
+  while(workList.length){
+    var pos = workList.pop();
+    var offset = pos.x + dataImage.width * pos.y;
+    if(alreadyFilled[offset]){
+      continue;
+    }else{
+      cx.fillRect(pos.x, pos.y, 1, 1);
+      console.log(controllaStessoColore(pos, pos, dataImage));
+      alreadyFilled[offset] = true;
+
+      coloraVicino(function(vicino){
+
+      });
+      
+    }
+      
+  }
+
+
+
+};
+
+
+
+/*
+// Call a given function for all horizontal and vertical neighbors
+// of the given point.
+function forAllNeighbors(point, fn) {
+  fn({x: point.x, y: point.y + 1});
+  fn({x: point.x, y: point.y - 1});
+  fn({x: point.x + 1, y: point.y});
+  fn({x: point.x - 1, y: point.y});
+}
+
+// Given two positions, returns true when they hold the same color.
+function isSameColor(data, pos1, pos2) {
+  var offset1 = (pos1.x + pos1.y * data.width) * 4;
+  var offset2 = (pos2.x + pos2.y * data.width) * 4;
+  for (var i = 0; i < 4; i++) {
+    if (data.data[offset1 + i] != data.data[offset2 + i])
+      return false;
+  }
+  return true;
+}
+
+
+tools["Flood fill"] = function(event, cx) {
+  var startPos = relativePos(event, cx.canvas);
+
+  var data = cx.getImageData(0, 0, cx.canvas.width,
+                              cx.canvas.height);
+  // An array with one place for each pixel in the image.
+  var alreadyFilled = new Array(data.width * data.height);
+
+  // This is a list of same-colored pixel coordinates that we have
+  // not handled yet.
+  var workList = [startPos];
+  
+
+  while (workList.length) {
+    var pos = workList.pop();
+    var offset = pos.x + data.width * pos.y;
+    if (alreadyFilled[offset]) continue;
+
+    cx.fillRect(pos.x, pos.y, 1, 1);
+    alreadyFilled[offset] = true;
+
+    forAllNeighbors(pos, function(neighbor) {
+      if (neighbor.x >= 0 && neighbor.x < data.width &&
+          neighbor.y >= 0 && neighbor.y < data.height &&
+          isSameColor(data, startPos, neighbor))
+        workList.push(neighbor);
+    });
+  }
+};
+*/
